9e7c4918be2f26931e5e72e65641cefe2787eb74
Vince Zhang <vincezzh@gmail.com>
2016-02-10 22:34:12 -0500

    adding animation in register step two page

--- beibang/beibang/RegisterStepTwoViewController.swift
+++ beibang/beibang/RegisterStepTwoViewController.swift
@@ -12,9 +12,22 @@ class RegisterStepTwoViewController: UIViewController, UIImagePickerControllerDe
 
     @IBOutlet weak var avatarSelectButton: UIButton!
     @IBOutlet weak var willBePregnantButton: UIButton!
+    @IBOutlet weak var willBePregnantLabel: UILabel!
+    @IBOutlet weak var willBePregnantDoneButton: UIButton!
+    @IBOutlet weak var willBePregnantView: UIView!
     @IBOutlet weak var willBeMomButton: UIButton!
+    @IBOutlet weak var willBeMomLabel: UILabel!
+    @IBOutlet weak var willBeMomDateTextfield: UITextField!
+    @IBOutlet weak var willBeMomDoneButton: UIButton!
+    @IBOutlet weak var willBeMomView: UIView!
     @IBOutlet weak var hasBeenMomButton: UIButton!
-
+    @IBOutlet weak var hasBeenMomLabel: UILabel!
+    @IBOutlet weak var hasBeenMomBabyNameTextfield: UITextField!
+    @IBOutlet weak var hasBeenMomBabyBirthdayTextfield: UITextField!
+    @IBOutlet weak var hasBeenMomSetMainPageButton: UIButton!
+    @IBOutlet weak var hasBeenMomDoneButton: UIButton!
+    @IBOutlet weak var hasBeenMomView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,12 +40,19 @@ class RegisterStepTwoViewController: UIViewController, UIImagePickerControllerDe
         
         willBePregnantButton.layer.cornerRadius = 0.5 * willBePregnantButton.bounds.size.width
         willBePregnantButton.clipsToBounds = true
+        willBePregnantDoneButton.layer.cornerRadius = 5
         
         willBeMomButton.layer.cornerRadius = 0.5 * willBeMomButton.bounds.size.width
         willBeMomButton.clipsToBounds = true
+        willBeMomDateTextfield.borderStyle = UITextBorderStyle.RoundedRect
+        willBeMomDoneButton.layer.cornerRadius = 5
         
         hasBeenMomButton.layer.cornerRadius = 0.5 * hasBeenMomButton.bounds.size.width
         hasBeenMomButton.clipsToBounds = true
+        hasBeenMomBabyNameTextfield.borderStyle = UITextBorderStyle.RoundedRect
+        hasBeenMomBabyBirthdayTextfield.borderStyle = UITextBorderStyle.RoundedRect
+        hasBeenMomSetMainPageButton.layer.cornerRadius = 5
+        hasBeenMomDoneButton.layer.cornerRadius = 5
     }
 
     @IBAction func selectAvatar(sender: AnyObject) {
@@ -76,4 +96,55 @@ class RegisterStepTwoViewController: UIViewController, UIImagePickerControllerDe
         UIGraphicsEndImageContext()
         return scaledimage
     }
+    
+    @IBAction func clickWillBePregnantButton(sender: AnyObject) {
+        UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                self.willBeMomButton.alpha = 0
+                self.willBeMomLabel.alpha = 0
+                self.hasBeenMomButton.alpha = 0
+                self.hasBeenMomLabel.alpha = 0
+            }) { (result: Bool) -> Void in
+                UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    let screenCenterX = UIScreen.mainScreen().bounds.width / 2
+                    self.willBePregnantButton.center = CGPointMake(screenCenterX, self.willBePregnantButton.center.y)
+                    self.willBePregnantLabel.center = CGPointMake(screenCenterX, self.willBePregnantLabel.center.y)
+                    self.willBePregnantView.center = CGPointMake(self.willBePregnantView.center.x, self.willBePregnantView.center.y - 91)
+                    }, completion: nil)
+        }
+    }
+    
+    @IBAction func clickWillBeMomButton(sender: AnyObject) {
+        UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.willBePregnantButton.alpha = 0
+            self.willBePregnantLabel.alpha = 0
+            self.hasBeenMomButton.alpha = 0
+            self.hasBeenMomLabel.alpha = 0
+            }) { (result: Bool) -> Void in
+                UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    self.willBeMomView.center = CGPointMake(self.willBeMomView.center.x, self.willBeMomView.center.y - 237)
+                    }, completion: nil)
+        }
+    }
+    
+    @IBAction func clickHasBeenMomButton(sender: AnyObject) {
+        UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.willBeMomButton.alpha = 0
+            self.willBeMomLabel.alpha = 0
+            self.willBePregnantButton.alpha = 0
+            self.willBePregnantLabel.alpha = 0
+            }) { (result: Bool) -> Void in
+                UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                    let screenCenterX = UIScreen.mainScreen().bounds.width / 2
+                    self.hasBeenMomButton.center = CGPointMake(screenCenterX, self.hasBeenMomButton.center.y)
+                    self.hasBeenMomLabel.center = CGPointMake(screenCenterX, self.hasBeenMomLabel.center.y)
+                    self.hasBeenMomView.center = CGPointMake(self.hasBeenMomView.center.x, self.hasBeenMomView.center.y - 307)
+                    }, completion: nil)
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
