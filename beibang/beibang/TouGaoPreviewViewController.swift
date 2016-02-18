d55ebd7a2d523ec684f01c0682b056ca6cc09623
Vince Zhang <vincezzh@gmail.com>
2016-02-17 23:29:22 -0500

    Initialize TouGaoPreviewViewController

---
+++ beibang/beibang/TouGaoPreviewViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TouGaoPreviewViewController.swift
+//  beibang
+//
+//  Created by Zhehan Zhang on 2016-02-17.
+//  Copyright © 2016 Akhaltech. All rights reserved.
+//
+
+import UIKit
+
+class TouGaoPreviewViewController: UIViewController {
+
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var followButton: UIButton!
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var itemLevelButton1: UIButton!
+    @IBOutlet weak var itemLevelButton2: UIButton!
+    @IBOutlet weak var itemLevelButton3: UIButton!
+    @IBOutlet weak var itemLevelButton4: UIButton!
+    @IBOutlet weak var itemLevelButton5: UIButton!
+    @IBOutlet weak var buyLinkButton: UIButton!
+    @IBOutlet weak var loveButton: UIButton!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var goBackButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
